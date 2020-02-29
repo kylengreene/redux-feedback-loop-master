88e7b6c88d3a32801753ef43429783230897ea2f
Kyle Greene <kylengreene@gmail.com>
2020-02-29 14:55:35 -0600

    removed superfluous files

--- server/server.js
+++ server/server.js
@@ -9,20 +9,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-const commentsRouter = require('./routes/comments.router');
-app.use('/comments', commentsRouter);
 
-const feelingRouter = require('./routes/feeling.router');
-app.use('/comments', feelingRouter);
 
 const submittedRouter = require('./routes/submitted.router');
 app.use('/comments', submittedRouter);
 
-const supportRouter = require('./routes/support.router');
-app.use('/comments', supportRouter);
-
-const understandingRouter = require('./routes/understanding.router');
-app.use('/comments', understandingRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
