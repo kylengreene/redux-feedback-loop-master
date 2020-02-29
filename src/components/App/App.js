dc657b7c07b5400c7397486544bae0941a52a7bb
Kyle Greene <kylengreene@gmail.com>
2020-02-29 11:45:40 -0600

    route paths written out in app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,30 @@ import axios from 'axios';
 import './App.css';
 import { connect } from 'react-redux';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Comments from '../Comments/Comments';
+import Feeling from '../Feeling/Feeling';
+import Review from '../Review/Review'
+import Submitted from '../Submitted/Submitted';
+import Support from '../Support/Support';
+import Understanding from '../Understanding/Understanding';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router>
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
+          <Route path="/Comments" component={Comments} />
+          <Route path="/Feeling" component={Feeling} />
+          <Route path="/Review" component={Review} />
+          <Route path="/Submitted" component={Submitted} />
+          <Route path="/Support" component={Support} />
+          <Route path="/Understanding" component={Understanding} />
+        </Router>
       </div>
     );
   }
